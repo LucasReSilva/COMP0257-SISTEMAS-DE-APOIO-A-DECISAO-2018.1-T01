e62b33c39d36efcc1f797970169f89e97dcea53f
Lucas Silva <lucasre.silva@gmail.com>
2018-07-24 22:05:50 -0300

    [Parcial] : Dados populacionais de 2017 das regiões e UFs

---
+++ src/data/Populacao.js
@@ -0,0 +1,110 @@
+export default {
+  Brasil: {
+    ano: {
+      2017: 207660929
+    },
+    regiao: {
+      Norte: {
+        ano: {
+          2017: 17936201
+        },
+        uf: {
+          Rondonia: {
+            ano: {
+              2017: 17936201
+            },
+            municipio: {}
+          },
+          Acre: {
+            ano: {
+              2017: 829619
+            },
+            municipio: {}
+          },
+          Amazonas: {
+            ano: {
+              2017: 4063614
+            },
+            municipio: {}
+          },
+          Roraima: {
+            ano: {
+              2017: 522636
+            },
+            municipio: {}
+          },
+          Pará: {
+            ano: {
+              2017: 8366628
+            },
+            municipio: {}
+          },
+          Amapá: {
+            ano: {
+              2017: 797722
+            },
+            municipio: {}
+          },
+          Tocantins: {
+            ano: {
+              2017: 1550194
+            },
+            municipio: {}
+          }
+        }
+      },
+      Nordeste: {
+        ano: {
+          2017: 57254159
+        },
+        uf: {
+          Maranhão: {
+            ano: {
+              2017: 7000229
+            },
+            municipio: {}
+          }
+        }
+      },
+      Sudeste: {
+        ano: {
+          2017: 86949714
+        },
+        uf: {
+          "Minas Gerais": {
+            ano: {
+              2017: 21119536
+            },
+            municipio: {}
+          }
+        }
+      },
+      Sul: {
+        ano: {
+          2017: 29644948
+        },
+        uf: {
+          Paraná: {
+            ano: {
+              2017: 11320892
+            },
+            municipio: {}
+          }
+        }
+      },
+      "Centro - Oeste": {
+        ano: {
+          2017: 15875907
+        },
+        uf: {
+          "Mato Grosso do Sul": {
+            ano: {
+              2017: 2713147
+            },
+            municipio: {}
+          }
+        }
+      }
+    }
+  }
+};
